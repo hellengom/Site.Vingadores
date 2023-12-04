e4fc34bf8e6403de027dd9657ef43df3adb9334c
hellen_muniz <132108994+hellengom@users.noreply.github.com>
2023-12-03 23:09:16 -0300

    Add files via upload

---
+++ SITE_PROVA_WEB_2ETAPA/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".carrosel").bxSlider({
+      auto: true,
+      speed: 1000,
+      adaptiveHeight: true,
+      slideWidth: 600,
+      /*mode: 'fade'*/
+      pager: false
+  })
+});
+
